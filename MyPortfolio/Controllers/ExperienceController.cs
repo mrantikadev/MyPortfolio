d63c79a4b403f58a665e23efd04f53fc83e8b55c
mrantikadev <94685047+mrantikadev@users.noreply.github.com>
2025-03-03 14:57:18 +0300

    Experience List

--- MyPortfolio/Controllers/ExperienceController.cs
+++ MyPortfolio/Controllers/ExperienceController.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DAL.Context;
 
 namespace MyPortfolio.Controllers
 {
     public class ExperienceController : Controller
     {
+        private readonly AppDbContext _context;
+
+        public ExperienceController(AppDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult ExperienceList()
         {
-            return View();
+            var values = _context.Experiences.ToList();
+            return View(values);
         }
     }
 }
