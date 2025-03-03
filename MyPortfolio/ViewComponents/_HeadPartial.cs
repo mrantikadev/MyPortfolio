97573245305f48b54fe7db59c5a19114ea1d7304
mrantikadev <94685047+mrantikadev@users.noreply.github.com>
2025-03-03 11:59:38 +0300

    View Components

---
+++ MyPortfolio/ViewComponents/_HeadPartial.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyPortfolio.ViewComponents
+{
+    public class _HeadPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+
+
+            return View();
+        }
+    }
+}
