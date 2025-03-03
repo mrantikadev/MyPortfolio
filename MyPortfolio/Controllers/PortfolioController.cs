7460c79c47022f804f5e841ecb0aa25c262a03d4
mrantikadev <94685047+mrantikadev@users.noreply.github.com>
2025-03-03 15:53:27 +0300

    Portfolio CRUD

---
+++ MyPortfolio/Controllers/PortfolioController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DAL.Context;
+using MyPortfolio.DAL.Entities;
+
+namespace MyPortfolio.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public PortfolioController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult PortfolioList()
+        {
+            var values = _context.Portfolios.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult InsertPortfolio()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult InsertPortfolio(Portfolio portfolio)
+        {
+            _context.Portfolios.Add(portfolio);
+            _context.SaveChanges();
+
+            return RedirectToAction("PortfolioList");
+        }
+
+        public IActionResult DeletePortfolio(int id)
+        {
+            var value = _context.Portfolios.Find(id);
+            _context.Portfolios.Remove(value);
+            _context.SaveChanges();
+
+            return RedirectToAction("PortfolioList");
+        }
+
+        [HttpGet]
+        public IActionResult UpdatePortfolio(int id)
+        {
+            var value = _context.Portfolios.Find(id);
+
+            return View(value);
+        }
+
+        [HttpPost]
+        public IActionResult UpdatePortfolio(Portfolio portfolio)
+        {
+            _context.Portfolios.Update(portfolio);
+            _context.SaveChanges();
+
+            return RedirectToAction("PortfolioList");
+        }
+    }
+}
