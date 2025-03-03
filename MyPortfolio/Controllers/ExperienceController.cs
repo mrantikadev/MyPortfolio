29fbdc091df8668752adb9f74b808c69d3cfc41d
mrantikadev <94685047+mrantikadev@users.noreply.github.com>
2025-03-03 15:31:58 +0300

    Add and Delete Experience

--- MyPortfolio/Controllers/ExperienceController.cs
+++ MyPortfolio/Controllers/ExperienceController.cs
@@ -33,5 +33,31 @@ namespace MyPortfolio.Controllers
 
             return RedirectToAction("ExperienceList");
         }
+
+        public IActionResult DeleteExperience(int id)
+        {
+            var value = _context.Experiences.Find(id);
+            _context.Experiences.Remove(value);
+            _context.SaveChanges();
+
+            return RedirectToAction("ExperienceList");
+        }
+
+        [HttpGet]
+        public IActionResult UpdateExperience(int id)
+        {
+            var value = _context.Experiences.Find(id);
+
+            return View(value);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateExperience(Experience experience)
+        {
+            _context.Experiences.Update(experience);
+            _context.SaveChanges();
+
+            return RedirectToAction("ExperienceList");
+        }
     }
 }
